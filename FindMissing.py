d624a15b735b95006813c30ba16df4fcd34aee9e
raymo116 <raymo116@mail.chapman.edu>
2020-01-18 17:19:10 -0800

    updating photos

--- FindMissing.py
+++
@@ -1,6 +0,0 @@
-import os
-
-for filename in os.listdir("./cropped_Astronaut_photos"):
-    if filename.endswith(".jpg") or filename.endswith(".png"):
-        if filename[8:] not in os.listdir("./ISS_Astronaut_photos"):
-            print(filename)
