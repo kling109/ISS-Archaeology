73d3d31f6095240d42c9e934f8a7f4709b746a84
brisenodaniel <brise107@mail.chapman.edu>
2020-01-23 14:00:52 -0800

    Adds documentation

--- Facial_Recognition_Model/Main.py
+++ Facial_Recognition_Model/Main.py
@@ -1,6 +1,6 @@
 import Main_Model
 
-model = Main_Model.Main_Model(r"../cropped_Astronaut_photos/",r'./pickles_bin/')
+model = Main_Model.Master_Model(r"../cropped_Astronaut_photos/",r'./pickles_bin/')
 model.findFacesDir("../Test/")
 print("5")
 d = model.found_faces
