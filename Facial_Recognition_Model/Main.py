0e9df9c38f36e9b43f3ac7208aefeb16e868f37a
Trevor Kling <kling109@mail.chapman.edu>
2020-01-23 15:03:24 -0800

    Set up program to work on subdirectories

--- Facial_Recognition_Model/Main.py
+++ Facial_Recognition_Model/Main.py
@@ -1,7 +1,11 @@
 import Main_Model
+import os
 
 model = Main_Model.Master_Model(r"../cropped_Astronaut_photos/",r'./pickles_bin/')
-model.findFacesDir("../Test/")
-print("5")
+folders_list = os.walk("./ISS_Photos")
+folders = [item[0] for item in folder_list]
+for f in folders:
+    model.findFacesDir("./ISS_Photos/" + f)
+print(len(folders))
 d = model.found_faces
 print(d)
