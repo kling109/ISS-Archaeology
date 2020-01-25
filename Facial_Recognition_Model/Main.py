f7bf7d9b57691f0a39fce083c3ab3a8ea5f905a6
brisenodaniel <brise107@mail.chapman.edu>
2020-01-25 04:04:00 -0800

    Implements parallelism in both training and analysis

--- Facial_Recognition_Model/Main.py
+++ Facial_Recognition_Model/Main.py
@@ -2,7 +2,7 @@ import Main_Model
 import os
 
 if __name__ == '__main__':
-    model = Main_Model.Master_Model(r"../cropped_Astronaut_photos/",r'./pickles_bin/',4)
+    model = Main_Model.Master_Model(r"../cropped_Astronaut_photos/",r'./pickles_bin/',5)
     model.findFacesDir("../Test/")
     d = model.found_faces
     for k,v in d.items():
