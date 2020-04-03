4098c513bb54b48d36f420dbd65e2f186d3aa491
raymo116 <raymo116@mail.chapman.edu>
2020-04-03 04:01:48 -0400

    starting code cleanup

--- Facial_Recognition_Model/Main.py
+++
@@ -1,9 +0,0 @@
-import Main_Model
-import os
-
-if __name__ == '__main__':
-    model = Main_Model.Master_Model(r"../cropped_Astronaut_photos/",r'./pickles_bin/',5)
-    model.findFacesDir("../Test/")
-    d = model.found_faces
-    for k,v in d.items():
-        print(k,"matched with:",v)
